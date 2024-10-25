39fb7b5f63094ec312fa4f2f9e4700c151798bb4
Alexandre Mesmacque <alexandre.mesmacque2024@campus-enif.fr>
2024-10-25 16:32:48 +0200

    First commit

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,28 +2,81 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 
-
-
 {
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9\-]+'])]
-    public function show(string $slug, int $id): Response
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
     {
-        return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id,
+        $recipes = $repository->findWithDurationLowerThan(30);
+        $recipes[0]->setTitle('Pâtes bolognaise');
+        $em->flush();
+        return $this->render('recipe/index.html.twig',[
+        'recipes' => $recipes
         ]);
     }
 
 
-    #[Route('/recettes', name: 'recipe.index')]
-    public function index(): Response
+
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9\-]+'])]
+    public function show(string $slug, int $id, RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig');
+
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+
+
+
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
     }
+
+    #[Route('/recettes/{id}/edit', name: 'recipe.edit')]
+    function edit(Recipe $recipe, Request $request, EntityManagerInterface $em): Response {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           $em->flush();
+           $this->addFlash('alert-success', 'La recette à bien été modifiée');
+           return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('recipe/edit.html.twig', ['recipe' => $recipe ,'form' => $form ]);
+
+    }
+
+    #[Route('/recettes/create', name: 'recipe.create')]
+    function create(Request $request, EntityManagerInterface $em)
+
+    {
+
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class,$recipe );
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($recipe);
+        $em->flush();
+        $this->addFlash('success', 'la recette a bien été créee');
+        return $this->redirectToRoute('recipe.index');
+
+        }
+
+        return $this->render('recipe/create.html.twig', [
+
+            'form'=> $form
+        ]);
+    }
+
 }
